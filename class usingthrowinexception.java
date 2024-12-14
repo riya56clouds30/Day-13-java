3d7a669d0657ead9ef3606b4c19b24f3a92c32a4
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-14 15:27:54 +0530

    Add files via upload

---
+++ class usingthrowinexception.java
@@ -0,0 +1,13 @@
+public class usingthrowinexception
+{
+     public static void main(String args[])
+{
+    try{
+        Throw new exception("error");
+    }
+    catch (exception)
+    {
+        System.out.println(e.getmesseage());
+    }
+    }
+}
